e652aff94a31d45a3a482a468756c4050c76cae5
Franco Desunvila <fdesuveron@hotmail.com>
2025-07-06 20:32:33 -0300

    whitenoise

--- conciliador_web/settings.py
+++ conciliador_web/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
 # 🧩 Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -77,6 +78,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'procesador', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # 🆔 PK automático por defecto
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
