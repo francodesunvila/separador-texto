ea08eead1eacac31aeacf19ad56a2b672c39a22f
Franco Desunvila <fdesuveron@hotmail.com>
2025-07-07 03:24:26 -0300

    fixurl

--- procesador/urls.py
+++ procesador/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('descargar_excel/', views.descargar_excel, name='descargar_excel'),
+    path('descargar_directo/<int:bloque_id>/', views.descargar_directo, name='descargar_directo'),
     path('eliminar_preview/', views.eliminar_preview, name='eliminar_preview'),
 ]
